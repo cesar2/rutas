df3f7ff640a1f3c979f7c87ef5b2bc23c511a174
Cesar Albusac <cesypozo@correo.ugr.es>
2020-11-28 20:20:26 +0100

    Añadiendo filtro en rutas

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { PostI } from '../../../shared/models/post.interface';
 import { PostService } from './../../posts/post.service';
 
@@ -10,12 +11,27 @@ import { PostService } from './../../posts/post.service';
 })
 export class HomeComponent implements OnInit {
   public posts$: Observable<PostI[]>;
+  public posts_all$: Observable<PostI[]>;
   constructor(private postSvc: PostService){
 
   }
 
   ngOnInit(){
     this.posts$ = this.postSvc.getAllPost();
+    this.posts_all$ = this.postSvc.getAllPost();
+  }
+
+  applyFilter(event: any) {
+    var fil =  event.target.value.trim().toLowerCase();
+    if(fil == "")
+    {
+      this.posts$ = this.posts_all$;
+    }
+    else
+    {
+      this.posts$ = this.posts$.pipe(map(posts => posts.filter(post => post.titlePost.trim().toLowerCase().includes(fil))));
+    }
+    
   }
  
 }
